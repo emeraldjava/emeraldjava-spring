7d8e60e68ddf15524ce139cfcb3280df524d7391
paul.oconnell <paul.oconnell@bearingpoint.com>
2017-02-26 14:18:16 +0000

    Add thymeleaf module

---
+++ thymeleaf/src/main/java/ie/emeraldjava/springboot/thymeleaf/IndexController.java
@@ -0,0 +1,24 @@
+package ie.emeraldjava.springboot.thymeleaf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * Created by pauloconnell on 26/02/17.
+ */
+@Controller
+public class IndexController {
+
+    // inject via application.properties
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "index";
+    }
+}
