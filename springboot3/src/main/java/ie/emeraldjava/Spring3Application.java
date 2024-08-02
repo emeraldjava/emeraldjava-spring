9d1ddcd6f5525c4977d9e464459335fd22a400a4
emeraldjava <emeraldjava@protonmail.com>
2024-08-02 20:07:39 +0100

    Initial spring3 app.

---
+++ springboot3/src/main/java/ie/emeraldjava/Spring3Application.java
@@ -0,0 +1,19 @@
+package ie.emeraldjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
+public class Spring3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3Application.class, args);
+        System.out.println("Hello world!");
+    }
+}
